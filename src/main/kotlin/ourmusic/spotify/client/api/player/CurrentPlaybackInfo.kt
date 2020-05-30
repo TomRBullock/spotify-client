e253b63660aa6db996284b4ec273a8345ebb3345
Thomas <thomas@lindar.com>
2020-05-30 18:59:39 +0100

    Add player mechanics

---
+++ src/main/kotlin/ourmusic/spotify/client/api/player/CurrentPlaybackInfo.kt
@@ -0,0 +1,27 @@
+package ourmusic.spotify.client.api.player
+
+import com.wrapper.spotify.SpotifyApi
+import com.wrapper.spotify.exceptions.SpotifyWebApiException
+import com.wrapper.spotify.model_objects.miscellaneous.CurrentlyPlayingContext
+import ourmusic.spotify.client.api.BaseService
+import java.io.IOException
+
+
+class CurrentPlaybackInfo: BaseService() {
+
+
+
+    fun getInformationAboutUsersCurrentPlayback_Sync(accessToken: String): CurrentlyPlayingContext? {
+
+        val spotifyApi = SpotifyApi.Builder()
+                .setAccessToken(accessToken)
+                .build()
+        val getInformationAboutUsersCurrentPlaybackRequest = spotifyApi.informationAboutUsersCurrentPlayback //                  .market(CountryCode.SE)
+                //                  .additionalTypes("track,episode")
+                .build()
+
+        return executeRequest(getInformationAboutUsersCurrentPlaybackRequest)
+    }
+
+
+}
--- src/main/kotlin/ourmusic/spotify/client/api/player/SkipTrack.kt
+++ src/main/kotlin/ourmusic/spotify/client/api/player/SkipTrack.kt
@@ -6,19 +6,20 @@ import ourmusic.spotify.client.api.BaseService
 import java.io.IOException
 
 
-class AddTrackToQueue: BaseService() {
+class SkipTrack: BaseService() {
 
-
-    fun addItemToUsersPlaybackQueue_Sync(accessToken: String, trackUri: String) {
+    fun skipUsersPlaybackToNextTrack_Sync(accessToken: String) {
 
         val spotifyApi = SpotifyApi.Builder()
                 .setAccessToken(accessToken)
                 .build()
 
-        val addItemToUsersPlaybackQueueRequest = spotifyApi
-                .addItemToUsersPlaybackQueue(trackUri) //    .device_id("5fbb3ba6aa454b5534c4ba43a8c7e8e45a63ad0e")
+        val skipUsersPlaybackToNextTrackRequest = spotifyApi
+                .skipUsersPlaybackToNextTrack()
+//                .device_id("5fbb3ba6aa454b5534c4ba43a8c7e8e45a63ad0e")
                 .build()
 
-        executeRequest(addItemToUsersPlaybackQueueRequest)
+        executeRequest(skipUsersPlaybackToNextTrackRequest)
     }
+
 }
